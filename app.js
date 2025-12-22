294a93768a741e7a3defabe851adcc1da47c0579
Arsh Ali Khan <arshalik1111@gmail.com>
2025-12-22 16:22:20 +0530

    feat: implement update and destroy functionality for listings

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride("_method"));
 const MONGO_URL = "mongodb://127.0.0.1:27017/openExploro";
 main()
   .then(() => {
@@ -40,6 +41,27 @@ app.get("/listings/:id", async (req, res) => {
 app.post("/listings", async (req, res) => {
   let newListing = new Listing(req.body.listing);
   await newListing.save();
+  res.redirect("/listings");
+});
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect("/listings");
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+
   res.redirect("/listings");
 });
 app.get("/", (req, res) => {
