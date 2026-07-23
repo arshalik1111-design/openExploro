469e1a9b1f9992872e359a7bd685bec3cf10ff31
Arsh Ali Khan <arshalik1111@gmail.com>
2026-07-23 20:33:57 +0530

    Restructred app.js

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 const express = require("express");
 const app = express();
-process.setMaxListeners(20); // Increase from 10 to 20
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
-const Review = require("./models/Review.js");
-
-const ejsMate = require("ejs-mate");
 const path = require("path");
-app.use(express.urlencoded({ extended: true }));
-
 const methodOverride = require("method-override");
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
-const wrapAsync = require("./utils/wrapAsync.js");
+const ejsMate = require("ejs-mate");
+
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("./schema.js");
-const review = require("./models/review.js");
-const { log } = require("console");
+
+// Import Routers
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
 
+// Database Connection
 const MONGO_URL = "mongodb://127.0.0.1:27017/openExploro";
 main()
   .then(() => {
@@ -31,27 +20,41 @@ main()
   .catch((err) => {
     console.log(err);
   });
+
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+// View Engine & Static Files Setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Hi I'm root");
 });
 
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
 
 app.all("*splat", (req, res, next) => {
   next(new ExpressError(404, "Page not Found"));
-})
+});
+
+// Global Error Handler
 app.use((err, req, res, next) => {
   // Set default values in case err.statusCode or err.message are missing
   let { statusCode = 500, message = "Something went wrong" } = err;
   res.status(statusCode).render("error.ejs", { message });
-  // res.status(statusCode).send(message);
 });
+
+// Server Listener
 app.listen("8080", () => {
   console.log("server is listning to port 8080");
 });
--- routes/review.js
+++ routes/review.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const { reviewSchema } = require("../schema.js");
-const review = require("../models/review.js");
+const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
 
 
@@ -21,6 +21,7 @@ const validateReview = (req, res, next) => {
 // Reviews Routes 
 //  Post Reviews route
 router.post("/", validateReview, wrapAsync(async (req, res) => {
+    console.log
     // First we get the id of the listing in whcih we have to add review
     let listing = await Listing.findById(req.params.id);
     // We create a newReview, and pass review in it that came from show.ejs
